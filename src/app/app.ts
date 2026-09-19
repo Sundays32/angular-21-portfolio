3011d2d050e8c3f3b8644166ecdec60c7f47fb7e
Sandesh <sandeshrao2000@gmail.com>
2026-09-19 22:40:58 +0530

    Header init commit and download cv button function

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('angular-21-portfolio');
+
+  public downloadCV(){
+    const link = document.createElement('a');
+
+    link.href = 'assets/Sandesh Rao Udupi -Resume.pdf';
+    link.download = 'Sandesh-Rao-CV.pdf';
+
+    link.click();
+  }
 }
