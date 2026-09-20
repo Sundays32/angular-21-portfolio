0b5c0fefd67d4652661dcfe295aa969a904dccdb
Sandesh <sandeshrao2000@gmail.com>
2026-09-20 21:26:01 +0530

    Banner section commit 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { Header } from './header/header';
+import { PortfolioPage } from './portfolio-page/portfolio-page';
 
 @Component({
-  imports: [RouterOutlet, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule, Header, PortfolioPage],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
@@ -12,12 +14,4 @@ import {MatButtonModule} from '@angular/material/button';
 export class App {
   protected readonly title = signal('angular-21-portfolio');
 
-  public downloadCV(){
-    const link = document.createElement('a');
-
-    link.href = 'assets/Sandesh Rao Udupi -Resume.pdf';
-    link.download = 'Sandesh-Rao-CV.pdf';
-
-    link.click();
-  }
 }
---
+++ src/app/portfolio-page/portfolio-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Banner } from '../banner/banner';
+
+@Component({
+  imports: [Banner],
+  selector: 'app-portfolio-page',
+  styleUrl: './portfolio-page.scss',
+  templateUrl: './portfolio-page.html',
+})
+export class PortfolioPage {}
