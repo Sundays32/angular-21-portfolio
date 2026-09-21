29fbaaf0eee261c1073ae0d25d99c94a15e4be5e
Sandesh <sandeshrao2000@gmail.com>
2026-09-21 14:28:01 +0530

    Contact and socials buttons

--- src/app/banner/banner.ts
+++ src/app/banner/banner.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips'
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  imports: [MatChipsModule],
+  imports: [MatChipsModule, MatIconModule, MatButtonModule],
   selector: 'app-banner',
   styleUrl: './banner.scss',
   templateUrl: './banner.html',
 })
-export class Banner {}
+export class Banner {
+
+  public emailMe():void{
+    const recipient = 'sandeshrao2000@gmail.com';
+    const url = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(recipient)}`;
+    
+    // Opens Gmail compose page in a new browser tab
+    window.open(url, '_blank');
+  }
+}
